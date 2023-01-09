bfc25af40d3114e4a85b9b1abfe97ca21c4fcfd6
zarlie1 <118620474+zarlie1@users.noreply.github.com>
2023-01-09 12:50:37 -0800

    Update sonar-project.js

--- sonar-project.js
+++ sonar-project.js
@@ -1,7 +1,7 @@
 const sonarqubeScanner = require('sonarqube-scanner');
 
 sonarqubeScanner({
-  serverUrl: 'http://34.209.65.180:9000/',
+  serverUrl: 'http://172.31.7.215:9000/',
        options : {
 	    'sonar.projectDescription': 'This is a Node JS application',
 	    'sonar.projectName': 'Node JS Application - Sample',
